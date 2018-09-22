4406541ef5c3cd0bbeb3d6f24e213681d2a61ecb
jaimelias <escribeme@jaimelias.com>
2018-09-22 19:00:33 +0000

    fix window load

--- public/js/cf7pd-public.js
+++ public/js/cf7pd-public.js
@@ -1,115 +1,124 @@
 (function( $ ) {
 	'use strict';
 
-	$(function(){
-		
-		$(window).load(function(){
-			var htmllang = $("html").attr("lang");
-			htmllang = htmllang.slice(0, -3);		
-			var pipedrive_url = cf7pd_url();
-			pipedrive_country_list(pipedrive_url, htmllang);
-			pipedrive_lang(htmllang);
-			pipedrive_param();
-			pipedrive_id();
-			pipedrive_cookies();
-			responsive_datepicker();
-			responsive_timepicker();		
-		});		
-	});	
+	var htmllang = $("html").attr("lang");
+	htmllang = htmllang.slice(0, -3);		
+	var pipedrive_url = cf7pd_url();
+	pipedrive_country_list(pipedrive_url, htmllang);
+	pipedrive_lang(htmllang);
+	pipedrive_param();
+	pipedrive_id();
+	pipedrive_cookies();
+	responsive_datepicker();
+	responsive_timepicker();
+	
 })( jQuery );
 
 function pipedrive_cookies()
 {
-	var landing = ['channel', 'device', 'landing_domain', 'landing_path'];
-	var warnings = 0;		
-	
-	$('.wpcf7-form').each(function(){
+	$(window).load(function(){
+		var landing = ['channel', 'device', 'landing_domain', 'landing_path'];
+		var warnings = 0;		
 		
-		var this_form = $(this);
-		
-		for(var x = 0; x < landing.length; x++)
-		{
-			var this_field = $(this_form).find('input.'+landing[x]);
+		$('.wpcf7-form').each(function(){
 			
-			$(this_field).each(function(){
-				$(this).val(getCookie(landing[x]));
-			});
+			var this_form = $(this);
 			
-			if($(this_field).length == 0)
+			for(var x = 0; x < landing.length; x++)
 			{
-				console.warn('input.'+landing[x]+' not found');
-				warnings++;
+				var this_field = $(this_form).find('input.'+landing[x]);
+				
+				$(this_field).each(function(){
+					$(this).val(getCookie(landing[x]));
+				});
+				
+				if($(this_field).length == 0)
+				{
+					console.warn('input.'+landing[x]+' not found');
+					warnings++;
+				}
 			}
-		}
+		});
+		
+		if(warnings > 0)
+		{
+			console.warn('You can create custom fields with Pipedrive and track metrics.');
+		}		
 	});
-	
-	if(warnings > 0)
-	{
-		console.warn('You can create custom fields with Pipedrive and track metrics.');
-	}
 }
 
 function pipedrive_id(){
 	
-	$('.wpcf7-form').find('input.id').each(function(){
-		if(this.id != '')
-		{	
-			if($('[data-id="'+this.id+'"]').text() != '')
-			{
-				var cleantext = $('<div></div>').html($('[data-id="'+this.id+'"]').html()).text();
-				$(this).val($('[data-id="'+this.id+'"]').text());
-				console.log(this.id+': '+$(this).val());
+	$(window).load(function(){
+		$('.wpcf7-form').find('input.id').each(function(){
+			if(this.id != '')
+			{	
+				if($('[data-id="'+this.id+'"]').text() != '')
+				{
+					var cleantext = $('<div></div>').html($('[data-id="'+this.id+'"]').html()).text();
+					$(this).val($('[data-id="'+this.id+'"]').text());
+					console.log(this.id+': '+$(this).val());
+				}
+				else
+				{
+					console.log('data-id '+ this.id +' not found in HTML.');
+				}
+				
 			}
-			else
-			{
-				console.log('data-id '+ this.id +' not found in HTML.');
-			}
-			
-		}
-	});			
+		});		
+	});
 }
 function pipedrive_param()
 {
-	var urlParams = new URLSearchParams(window.location.search);
-	
-	$('.wpcf7-form').find('input.param').each(function(){
-		if(this.id != '')
-		{
-			if(urlParams.get(this.id) != '')
+	$(window).load(function(){
+		var urlParams = new URLSearchParams(window.location.search);
+		
+		$('.wpcf7-form').find('input.param').each(function(){
+			if(this.id != '')
 			{
-				$(this).val(urlParams.get(this.id));
-				console.log(this.id+': '+$(this).val());	
+				if(urlParams.get(this.id) != '')
+				{
+					$(this).val(urlParams.get(this.id));
+					console.log(this.id+': '+$(this).val());	
+				}
+				else
+				{
+					console.log('param '+ this.id +' not found in URL.');
+				}
 			}
-			else
-			{
-				console.log('param '+ this.id +' not found in URL.');
-			}
-		}
-	});	
+		});		
+	});
 }
 
 function pipedrive_lang(lang)
 {
-	$('.wpcf7-form').find('input.lang').each(function(){
-		$(this).val(lang);
-		console.log('lang: '+ $(this).val());
-	});	
+	$(window).load(function(){
+		$('.wpcf7-form').find('input.lang').each(function(){
+			$(this).val(lang);
+			console.log('lang: '+ $(this).val());
+		});			
+	});
 }
 
 function pipedrive_country_list(pluginurl, htmllang)
 {
-	$.getJSON( pluginurl + 'countries/'+htmllang+'.json')
-		.done(function(data) 
+	$(window).load(function(){
+		if($('.countrylist').length > 0)
 		{
-			pipedrive_country_options(data);
-		})
-		.fail(function()
-		{
-			$.getJSON(pluginurl + 'public/countries/en.json', function(data) {
+			$.getJSON( pluginurl + 'countries/'+htmllang+'.json')
+				.done(function(data) 
+				{
+					pipedrive_country_options(data);
+				})
+				.fail(function()
+				{
+					$.getJSON(pluginurl + 'public/countries/en.json', function(data) {
 
-				pipedrive_country_options(data);
-			});				
-		});			
+						pipedrive_country_options(data);
+					});				
+				});			
+		}
+	});
 }	
 
 function pipedrive_country_options(data)
@@ -119,39 +128,43 @@ function pipedrive_country_options(data)
 		{
 			$(this).append('<option value=' + data[x][0] + '>' + data[x][1] + '</option>');
 		}
-	});		
+	});
 }
 
 function responsive_datepicker()
 {
-	$('.wpcf7-form').find('input.datepicker').each(function()
-	{	
-		var args = {};
-		args.format = 'yyyy-mm-dd';
-		args.container = '#cf7pd-datepicker';
-		
-		if($(this).attr('type') == 'text')
-		{
-			$(this).pickadate(args);
-		}
-		else if($(this).attr('type') == 'date')
-		{
-			$(this).attr({'type': 'text'});
-			$(this).pickadate(args);
-		}
-	});		
+	$(window).load(function(){
+		$('.wpcf7-form').find('input.datepicker').each(function()
+		{	
+			var args = {};
+			args.format = 'yyyy-mm-dd';
+			args.container = '#cf7pd-datepicker';
+			
+			if($(this).attr('type') == 'text')
+			{
+				$(this).pickadate(args);
+			}
+			else if($(this).attr('type') == 'date')
+			{
+				$(this).attr({'type': 'text'});
+				$(this).pickadate(args);
+			}
+		});			
+	});	
 }
 
 
 function responsive_timepicker()
 {
-	var args = {};
-	args.container = '#cf7pd-timepicker';
-	
-	$('.wpcf7-form').find('input.timepicker').each(function()
-	{
-		$(this).pickatime(args);
-	});		
+	$(window).load(function(){
+		var args = {};
+		args.container = '#cf7pd-timepicker';
+		
+		$('.wpcf7-form').find('input.timepicker').each(function()
+		{
+			$(this).pickatime(args);
+		});			
+	});	
 }	
 
 
